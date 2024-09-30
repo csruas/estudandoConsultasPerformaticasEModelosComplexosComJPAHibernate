e4c9fed94b2540f3286c1d706724e08ff418c9fa
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2024-09-30 13:46:01 -0300

    =usando a API de critera

--- src/main/java/br/com/alura/loja/testes/TesteCriteria.java
+++ src/main/java/br/com/alura/loja/testes/TesteCriteria.java
@@ -1,30 +1,25 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.dao.CategoriaDao;
-import br.com.alura.loja.dao.ClienteDao;
-import br.com.alura.loja.dao.PedidoDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Cliente;
-import br.com.alura.loja.modelo.ItemPedido;
-import br.com.alura.loja.modelo.Pedido;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
-public class PerformanceConsultas {
-
+public class TesteCriteria {
+	
 	public static void main(String[] args) {
 		popularBancoDeDados();
 		EntityManager em = JPAUtil.getEntityManager();
-		PedidoDao pedidoDao = new PedidoDao(em);
-		Pedido pedido = pedidoDao.buscarPedidoComCliente(1l);
-		pedidoDao.buscarPedidoComCliente(1l);
-		em.close();
-		System.out.println(pedido.getCliente().getNome());
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		List<Produto> produtos = produtoDao.buscarPorParametrosComCriteria("PS5", null, LocalDate.now());
+		produtos.forEach(p -> System.out.println(p.getNome()));
 	}
 
 	private static void popularBancoDeDados() {
@@ -36,20 +31,9 @@ public class PerformanceConsultas {
 		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
 		Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
 
-		Cliente cliente = new Cliente("Cristiano", "123456");
-
-		Pedido pedido = new Pedido(cliente);
-		pedido.adicionarItem(new ItemPedido(10, pedido, celular));
-		pedido.adicionarItem(new ItemPedido(40, pedido, videogame));
-
-		Pedido pedido2 = new Pedido(cliente);
-		pedido2.adicionarItem(new ItemPedido(2, pedido, macbook));
-
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
-		ClienteDao clienteDao = new ClienteDao(em);
-		PedidoDao pedidoDao = new PedidoDao(em);
 
 		em.getTransaction().begin();
 
@@ -60,11 +44,6 @@ public class PerformanceConsultas {
 		produtoDao.cadastrar(celular);
 		produtoDao.cadastrar(videogame);
 		produtoDao.cadastrar(macbook);
-
-		clienteDao.cadastrar(cliente);
-
-		pedidoDao.cadastrar(pedido);
-		pedidoDao.cadastrar(pedido2);
 
 		em.getTransaction().commit();
 		em.close();
